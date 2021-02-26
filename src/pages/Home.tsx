d5dde82bfddd082fb207ba5154845cb0dfb9dd37
Adeel Malik <adeelmalik1988@gmail.com>
2021-02-26 19:57:51 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button,Typography, Card } from '@material-ui/core';
+import { Typography, Card } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom'
 
